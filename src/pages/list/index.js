90ae9dfbbd6553baab90110f8b2d7f03f8a884ec
longfengno1 <longfeng_no1@sina.com>
2019-11-13 03:20:52 +0800

    up list

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -3,8 +3,13 @@ import './index.scss';
 
 (function () {
   let expand = false;
+  let showPlace = false;
+
   function init() {
     $('#travel_calendar').hide();
+    $('#calendar-icon').addClass('icon-arrowup');
+    $('#calendar-icon').removeClass('icon-arrowdown');
+    $('#select-place-row').hide();
   }
 
   init();
@@ -12,10 +17,23 @@ import './index.scss';
   $('#show-calendar').click(() => {
     if (expand) {
       $('#travel_calendar').hide();
+      $('#calendar-icon').addClass('icon-arrowup');
+      $('#calendar-icon').removeClass('icon-arrowdown');
       expand = false;
     } else {
       $('#travel_calendar').show();
+      $('#calendar-icon').removeClass('icon-arrowup');
+      $('#calendar-icon').addClass('icon-arrowdown');
       expand = true
     }
+  })
+
+  $('#select-place').click(() => {
+    if (showPlace) {
+      $('#select-place-row').hide();
+    } else {
+      $('#select-place-row').show();
+    }
+    showPlace = !showPlace;
   })
 })();
