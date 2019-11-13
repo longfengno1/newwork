306bfd7fab3806460c239ad83fa5d89c5cc34abc
longfengno1 <longfeng_no1@sina.com>
2019-11-14 01:27:07 +0800

    up 客房

--- src/pages/fleetIntroduction01/index.js
+++ src/pages/fleetIntroduction01/index.js
@@ -1,2 +1,15 @@
+import '../../bootstrap/css/bootstrap.css';
+import '../../css/global2.scss';
 import "../../js/common";
-import "./index.css";
+import "./index.scss";
+
+(function () {
+  $(".topbanner_plane .ship .btn-icon").click(function () {
+    $(".topbanner_plane .ship .btn-icon").removeClass("artive");
+    $(this).addClass("artive");
+    return false;
+  })
+  $(".topbanner_plane").click(function () {
+    $(".topbanner_plane .ship .btn-icon").removeClass("artive");
+  })
+})();
