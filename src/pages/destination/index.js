d9b6419a53f1f3e2601a65e4a0f09a94fe1742fe
xugq979 <5281990@qq.com>
2019-12-04 07:20:19 +0800

    up for xugq

--- src/pages/destination/index.js
+++ src/pages/destination/index.js
@@ -1,2 +1,10 @@
 import "../../js/common2";
 import "./index.scss";
+(function () {
+    $(".filter_planeC .countrylist li").click(function () {
+        $(".filter_planeC .countrylist li").removeClass("current");
+        $(this).addClass("current");
+        return false;
+    })
+
+})()
