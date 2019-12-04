c931b8274fa7c74eeae7a52f79679d46b9d7c986
teller shen <shenhq@ctrip.com>
2019-12-05 02:55:43 +0800

    update

--- src/pages/xinshoudaolan/index.js
+++ src/pages/xinshoudaolan/index.js
@@ -1,2 +1,14 @@
 import "../../js/common2";
 import "./index.scss";
+$(document).ready(function () {
+    $(".filter_planeB").click(function(e){
+        var subview = $(this).attr("subview");
+        $("#"+subview).addClass("showView");
+
+        $("body").delay(1200).addClass("hiddenView");
+        $(".views .icon-arrowleft").click(function(e){
+            $("body").removeClass("hiddenView");
+            $(".views").removeClass("showView");
+        });
+    })
+});
