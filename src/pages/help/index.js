58c756e7ca8832266fba090398b787fd8d518cfa
xugq979 <5281990@qq.com>
2019-12-06 02:59:00 +0800

    up for xugq

--- src/pages/help/index.js
+++ src/pages/help/index.js
@@ -1,2 +1,21 @@
 import "../../js/common2";
 import "./index.scss";
+$(document).ready(function () {
+    $(".col-auto .filter_planeB").click(function (e) {
+
+        $(".col-auto .filter_planeC").removeClass("filter_planeC");
+        $(this).addClass("filter_planeC");
+
+        var subview = $(this).attr("subview");
+
+        $("#"+subview).addClass("showView");
+
+        $("body").delay(1200).addClass("hiddenView");
+        $(".views .icon-arrowleft").click(function(e){
+            $("body").removeClass("hiddenView");
+            $(".views").removeClass("showView");
+        });
+
+    })
+
+});
