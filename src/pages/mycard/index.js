6d7d2e96af18a4740677de8241318b4f16af686b
teller shen <shenhq@ctrip.com>
2019-12-06 19:56:35 +0800

    up

--- src/pages/mycard/index.js
+++ src/pages/mycard/index.js
@@ -26,3 +26,27 @@ import './index.scss';
 
     init();
 })();
+(function () {
+    $(document).ready(function () {
+        var TabBarBox = $("#userinfo-navbar");
+        var TBBWrap = $(".left-container");
+        $(window).bind("scroll", function () {
+            var st = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+            if (!TBBWrap.offset()) return;
+            if (st > TBBWrap.offset().top
+            && st < $(".left-container").offset().top+$(".left-container").height()-TabBarBox.height()) {
+                TabBarBox.addClass("boxfixed");
+                TabBarBox.css("width",TBBWrap.width());
+                TabBarBox.removeClass("boxbottom");
+            }else if(st < TBBWrap.offset().top){
+                TabBarBox.removeClass("boxfixed");
+                
+                TabBarBox.removeClass("boxbottom");
+            }else {
+                $(".left-container").css("position","relative")
+                TabBarBox.removeClass("boxfixed");
+                TabBarBox.addClass("boxbottom");
+            }
+        });
+    })
+})();
