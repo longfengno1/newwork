ef762260f9ba0c76fb2e115d715a0da15861f8a1
xugq979 <5281990@qq.com>
2019-12-10 23:36:43 +0800

    up for xugq

--- src/pages/activitysubjectDetail/index.js
+++ src/pages/activitysubjectDetail/index.js
@@ -17,5 +17,40 @@ import "./index.scss";
         $(this).addClass("current");
         return false;
     });
+
+
+    //选项卡切换
+    $('.tab-items ul li').click(function () {
+        $(this).addClass('current').siblings().removeClass('current');
+    }) 
+    //按钮箭头
+    var catew = $('.tab-items').width() - 120;
+    var cateLiw = 0;
+    $('.tab-items ul li').each(function () {
+        cateLiw += $(this).outerWidth();
+    })
+    //超出一屏幕，显示next
+    if (cateLiw < 1000) {
+        $('.next').hide();
+    }
+    var i = 0;
+    $('.next').click(function () {
+        $('.tab-items ul').animate({
+            "margin-left": -catew + 'px'
+        }, 500)
+        i++;
+        if ((catew + 120) * i + (catew + 120) >= cateLiw) {
+            $('.prev').show();
+            $('.next').hide();
+        }
+    })
+    $('.prev').click(function () {
+        $('.tab-items ul').animate({
+            "margin-left": 0 + 'px'
+        }, 500)
+        $(this).hide(); $('.next').show();
+    })
+
+
 })()
 
