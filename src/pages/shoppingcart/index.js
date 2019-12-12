af08807e8e7cef05ac486422f43c0eb25f700c35
teller shen <shenhq@ctrip.com>
2019-12-12 10:47:24 +0800

    up

--- src/pages/shoppingcart/index.js
+++ src/pages/shoppingcart/index.js
@@ -2,5 +2,15 @@ import '../../js/common2';
 import './index.scss';
 
 (function () {
-  
+    $(".mark").click(function(){
+        $(this).hide();
+        return false;
+    })
+    $(".mark .box").click(function(){
+       
+        return false;
+    })
+  $(".arrow").click(function(){
+      $(".mark").show();
+  })
 })();
