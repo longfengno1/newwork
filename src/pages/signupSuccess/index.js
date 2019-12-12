d1c3377991788c52b768f2752deec1fc6430049c
xugq979 <5281990@qq.com>
2019-12-12 09:47:36 +0800

    up for xugq

--- src/pages/signupSuccess/index.js
+++ src/pages/signupSuccess/index.js
@@ -1,2 +1,18 @@
-import '../../js/common';
-import './index.scss';
+import "../../js/common2";
+import "./index.scss";
+
+(function () {
+
+    $(".signin-type-tab").click(function () {
+        if (!$(this).hasClass("current")) {
+            $(".signin-type-tab").removeClass("current");
+            $(this).addClass("current");
+            $("#signin-mobile").toggle();
+            $("#signin-wx").toggle();
+        }
+
+    //$(this).parent().find(".signin-type-tab").toggleClass("current");
+    //$(this).parent().find(".signin-type-tab").toggle();
+})
+
+})()
