168c16332025371ed90ca665fedd22fb1cc45183
xugq979 <5281990@qq.com>
2019-12-19 20:34:13 +0800

    up

--- src/pages/booking6/index.js
+++ src/pages/booking6/index.js
@@ -2,42 +2,41 @@ import '../../js/common2';
 import './index.scss';
 
 (function () {
-  let currnetStep = 6;
-  let steps = ['selectDate', 'selectRoom', 'fillInfo', 'checkInfo', 'selectPay', 'orderSuccess'];
+    let steps = ['selectDate', 'selectRoom', 'fillInfo', 'checkInfo', 'selectPay', 'orderSuccess'];
 
-  function init() {
-    for (let i = 1; i <= 6; i++) {
-     
-      $(`#step${i}`).click(function(){
-        //selectCurrent(i);
-      })
+    function init() {
+        for (let i = 1; i <= 6; i++) {
+
+            $(`#step${i}`).click(function () {
+                //selectCurrent(i);
+            })
+        }
+        setStepStatus();
     }
-    setStepStatus();
-  }
 
-  function selectCurrent(index) {
-    currnetStep = index;
-    setStepStatus();
-  }
-
-  function setStepStatus() {
-    for (let i = 1; i <= 6; i++) {
-      if (currnetStep > i) {
-        $(`#step${i}`).removeClass('active');
-        $(`#step${i}`).addClass('selected');
-        $(`#${steps[i - 1]}`).hide();
-      } else if (currnetStep === i) {
-        $(`#step${i}`).removeClass('selected');
-        $(`#step${i}`).addClass('active');
-         $(`#${steps[i - 1]}`).show();
-      } else {
-        $(`#step${i}`).removeClass('selected');
-        $(`#step${i}`).removeClass('active');
-        $(`#${steps[i - 1]}`).hide();
-      }
+    function selectCurrent(index) {
+        currnetStep = index;
+        setStepStatus();
     }
-  }
 
-  init();
+    function setStepStatus() {
+        for (let i = 1; i <= 6; i++) {
+            if (currnetStep > i) {
+                $(`#step${i}`).removeClass('active');
+                $(`#step${i}`).addClass('selected');
+                $(`#${steps[i - 1]}`).hide();
+            } else if (currnetStep === i) {
+                $(`#step${i}`).removeClass('selected');
+                $(`#step${i}`).addClass('active');
+                $(`#${steps[i - 1]}`).show();
+            } else {
+                $(`#step${i}`).removeClass('selected');
+                $(`#step${i}`).removeClass('active');
+                $(`#${steps[i - 1]}`).hide();
+            }
+        }
+    }
+
+    init();
 
 })();
